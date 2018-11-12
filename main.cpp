5c9e32c206eeca68a71af23d2d39759a330e3482
Mike-415 <mgonz160@mail.ccsf.edu>
2018-11-12 15:04:34 -0800

    Added the simplify function

--- main.cpp
+++ main.cpp
@@ -40,6 +40,7 @@ private:
     int denominator;
     friend bool compareFractions(const Fraction& leftOperand, const Fraction& rightOperand, RelationalOperators type);
     friend Fraction addOrSubtract(const Fraction& leftOperand, const Fraction& rightOperand, ArithmeticOperators type);
+    void simplify(Fraction& original);
 };
 
 
@@ -52,6 +53,7 @@ Fraction::Fraction( int inNumerator, int inDenominator){
     assert(inDenominator != 0);
     numerator = inNumerator;
     denominator = inDenominator;
+    simplify(*this);
 }
 
 
@@ -63,12 +65,6 @@ std::ostream& operator<<(std::ostream& leftOutput, const Fraction& rightOperand)
     leftOutput << rightOperand.numerator << "/" << rightOperand.denominator;
     return leftOutput;
 }
-
-
-
-
-
-
 
 
 
@@ -311,11 +307,37 @@ Fraction Fraction::operator--(int){
 
 
 
-void BasicTest();
-void RelationTest();
-void BinaryMathTest();
-void MathAssignTest();
-string boolString(bool convertMe);
+/*
+void fraction::simplify() {
+    bool isNegative = fractionNegative();
+    int absNumerator = abs(numerator);
+    int smallestNum = (absNumerator < denominator)? absNumerator : denominator;
+    while( smallestNum > 1 ){
+        if(absNumerator % smallestNum == 0 &&
+           denominator  % smallestNum == 0){
+            absNumerator /= smallestNum;
+            denominator /= smallestNum;
+        }
+        smallestNum--;
+    }
+    numerator = (isNegative)? absNumerator * -1 : absNumerator;
+}
+ */
+
+//Assume that ONLY the numerator could be negative
+void Fraction::simplify(Fraction& original){
+    int absNumerator = std::abs(original.numerator);
+    int smallestNum = (absNumerator < original.denominator)? absNumerator : original.denominator;
+    while( smallestNum > 1 ){
+        if(absNumerator % smallestNum == 0 &&
+           original.denominator  % smallestNum == 0){
+            original.numerator /= smallestNum;
+            original.denominator /= smallestNum;
+        }
+        smallestNum--;
+    }
+}
+
 
 
 
@@ -325,292 +347,17 @@ string boolString(bool convertMe);
 
 int main()
 {
-    BasicTest();
-    RelationTest();
-    BinaryMathTest();
-    MathAssignTest();
+    Fraction f1(-9,3);
+    Fraction f2(5, 45);
+    cout << f1 << endl;
+    cout << f2 << endl;
 }
 
 
-
-
-
-
-void BasicTest()
-{
-    cout << "\n----- Testing basic Fraction creation & printing\n";
-
-    const Fraction fr[] = {Fraction(4, 8), Fraction(-15,21),
-                           Fraction(10), Fraction(12, -3),
-                           Fraction(), Fraction(28, 6), Fraction(0, 12)};
-
-    for (int i = 0; i < 7; i++){
-        cout << "Fraction [" << i <<"] = " << fr[i] << endl;
-    }
-}
-
-
-
-
-
-
-string boolString(bool convertMe) {
-    if (convertMe) {
-        return "true";
-    } else {
-        return "false";
-    }
-}
-
-
-
-
-
-
-void RelationTest()
-{
-    cout << "\n----- Testing relational operators between Fractions\n";
-
-    const Fraction fr[] =  {Fraction(3, 6), Fraction(1,2), Fraction(-15,30),
-                            Fraction(1,10), Fraction(0,1), Fraction(0,2)};
-
-    for (int i = 0; i < 5; i++) {
-        cout << "Comparing " << fr[i] << " to " << fr[i+1] << endl;
-        cout << "\tIs left < right? " << boolString(fr[i] < fr[i+1]) << endl;
-        cout << "\tIs left <= right? " << boolString(fr[i] <= fr[i+1]) << endl;
-        cout << "\tIs left > right? " << boolString(fr[i] > fr[i+1]) << endl;
-        cout << "\tIs left >= right? " << boolString(fr[i] >= fr[i+1]) << endl;
-        cout << "\tDoes left == right? " << boolString(fr[i] == fr[i+1]) << endl;
-        cout << "\tDoes left != right ? " << boolString(fr[i] != fr[i+1]) << endl;
-    }
-
-    cout << "\n----- Testing relations between Fractions and integers\n";
-    Fraction f(-3,6);
-    int num = 2;
-    cout << "Comparing " << f << " to " << num << endl;
-    cout << "\tIs left < right? " << boolString(f < num) << endl;
-    cout << "\tIs left <= right? " << boolString(f <= num) << endl;
-    cout << "\tIs left > right? " << boolString(f > num) << endl;
-    cout << "\tIs left >= right? " << boolString(f >= num) << endl;
-    cout << "\tDoes left == right? " << boolString(f == num) << endl;
-    cout << "\tDoes left != right ? " << boolString(f != num) << endl;
-
-    Fraction g(1,4);
-    num = -3;
-    cout << "Comparing " << num << " to " << g << endl;
-    cout << "\tIs left < right? " << boolString(num < g) << endl;
-    cout << "\tIs left <= right? " << boolString(num <= g) << endl;
-    cout << "\tIs left > right? " << boolString(num > g) << endl;
-    cout << "\tIs left >= right? " << boolString(num >= g) << endl;
-    cout << "\tDoes left == right? " << boolString(num == g) << endl;
-    cout << "\tDoes left != right ? " << boolString(num != g) << endl;
-}
-
-
-
-
-
-
-void BinaryMathTest()
-{
-    cout << "\n----- Testing binary arithmetic between Fractions\n";
-
-    const Fraction fr[] = {Fraction(1, 6), Fraction(1,3),
-                           Fraction(-2,3), Fraction(5), Fraction(-4,3)};
-
-    for (int i = 0; i < 4; i++) {
-        cout << fr[i] << " + " << fr[i+1] << " = " << fr[i] + fr[i+1] << endl;
-        cout << fr[i] << " - " << fr[i+1] << " = " << fr[i] - fr[i+1] << endl;
-        cout << fr[i] << " * " << fr[i+1] << " = " << fr[i] * fr[i+1] << endl;
-        cout << fr[i] << " / " << fr[i+1] << " = " << fr[i] / fr[i+1] << endl;
-    }
-
-    cout << "\n----- Testing arithmetic between Fractions and integers\n";
-    Fraction f(-1, 2);
-    int num = 4;
-    cout << f << " + " << num << " = " << f + num << endl;
-    cout << f << " - " << num << " = " << f - num << endl;
-    cout << f << " * " << num << " = " << f * num << endl;
-    cout << f << " / " << num << " = " << f / num << endl;
-
-    Fraction g(-1, 2);
-    num = 3;
-    cout << num << " + " << g << " = " << num + g << endl;
-    cout << num << " - " << g << " = " << num - g << endl;
-    cout << num << " * " << g << " = " << num * g << endl;
-    cout << num << " / " << g << " = " << num / g << endl;
-}
-
-
-
-
-
-
-void MathAssignTest()
-{
-    cout << "\n----- Testing shorthand arithmetic assignment on Fractions\n";
-
-    Fraction fr[] = {Fraction(1, 6), Fraction(4),
-                     Fraction(-1,2), Fraction(5)};
-
-    for (int i = 0; i < 3; i++) {
-        cout << fr[i] << " += " << fr[i+1] << " = ";
-        cout << (fr[i] += fr[i+1]) << endl;
-        cout << fr[i] << " -= " << fr[i+1] << " = ";
-        cout << (fr[i] -= fr[i+1]) << endl;
-        cout << fr[i] << " *= " << fr[i+1] << " = ";
-        cout << (fr[i] *= fr[i+1]) << endl;
-        cout << fr[i] << " /= " << fr[i+1] << " = ";
-        cout << (fr[i] /= fr[i+1]) << endl;
-    }
-
-    cout << "\n----- Testing shorthand arithmetic assignment using integers\n";
-    Fraction f(-1, 3);
-    int num = 3;
-    cout << f << " += " << num << " = ";
-    cout << (f += num) << endl;
-    cout << f << " -= " << num << " = ";
-    cout << (f -= num) << endl;
-    cout << f << " *= " << num << " = ";
-    cout << (f *= num) << endl;
-    cout << f << " /= " << num << " = ";
-    cout << (f /= num) << endl;
-
-    cout << "\n----- Testing increment/decrement prefix and postfix\n";
-    Fraction g(-1, 3);
-    cout << "Now g = " << g << endl;
-    cout << "g++ = " << g++ << endl;
-    cout << "Now g = " << g << endl;
-    cout << "++g = " << ++g << endl;
-    cout << "Now g = " << g << endl;
-    cout << "g-- = " << g-- << endl;
-    cout << "Now g = " << g << endl;
-    cout << "--g = " << --g << endl;
-    cout << "Now g = " << g << endl;
-}
 
 /*
 OUTPUT:
 
------ Testing basic Fraction creation & printing
-Fraction [0] = 4/8
-Fraction [1] = -15/21
-Fraction [2] = 10/1
-Fraction [3] = 12/-3
-Fraction [4] = 0/1
-Fraction [5] = 28/6
-Fraction [6] = 0/12
 
------ Testing relational operators between Fractions
-Comparing 3/6 to 1/2
-	Is left < right? false
-	Is left <= right? true
-	Is left > right? false
-	Is left >= right? true
-	Does left == right? true
-	Does left != right ? false
-Comparing 1/2 to -15/30
-	Is left < right? false
-	Is left <= right? false
-	Is left > right? true
-	Is left >= right? true
-	Does left == right? false
-	Does left != right ? true
-Comparing -15/30 to 1/10
-	Is left < right? true
-	Is left <= right? true
-	Is left > right? false
-	Is left >= right? false
-	Does left == right? false
-	Does left != right ? true
-Comparing 1/10 to 0/1
-	Is left < right? false
-	Is left <= right? false
-	Is left > right? true
-	Is left >= right? true
-	Does left == right? false
-	Does left != right ? true
-Comparing 0/1 to 0/2
-	Is left < right? false
-	Is left <= right? true
-	Is left > right? false
-	Is left >= right? true
-	Does left == right? true
-	Does left != right ? false
-
------ Testing relations between Fractions and integers
-Comparing -3/6 to 2
-	Is left < right? true
-	Is left <= right? true
-	Is left > right? false
-	Is left >= right? false
-	Does left == right? false
-	Does left != right ? true
-Comparing -3 to 1/4
-	Is left < right? true
-	Is left <= right? true
-	Is left > right? false
-	Is left >= right? false
-	Does left == right? false
-	Does left != right ? true
-
------ Testing binary arithmetic between Fractions
-1/6 + 1/3 = 9/18
-1/6 - 1/3 = -3/18
-1/6 * 1/3 = 1/18
-1/6 / 1/3 = 3/6
-1/3 + -2/3 = -3/9
-1/3 - -2/3 = 9/9
-1/3 * -2/3 = -2/9
-1/3 / -2/3 = 3/-6
--2/3 + 5/1 = 13/3
--2/3 - 5/1 = -17/3
--2/3 * 5/1 = -10/3
--2/3 / 5/1 = -2/15
-5/1 + -4/3 = 11/3
-5/1 - -4/3 = 19/3
-5/1 * -4/3 = -20/3
-5/1 / -4/3 = 15/-4
-
------ Testing arithmetic between Fractions and integers
--1/2 + 4 = 7/2
--1/2 - 4 = -9/2
--1/2 * 4 = -4/2
--1/2 / 4 = -1/8
-3 + -1/2 = 5/2
-3 - -1/2 = 7/2
-3 * -1/2 = -3/2
-3 / -1/2 = 6/-1
-
------ Testing shorthand arithmetic assignment on Fractions
-1/6 += 4/1 = 25/6
-25/6 -= 4/1 = 1/6
-1/6 *= 4/1 = 4/6
-4/6 /= 4/1 = 4/24
-4/1 += -1/2 = 7/2
-7/2 -= -1/2 = 16/4
-16/4 *= -1/2 = -16/8
--16/8 /= -1/2 = -32/-8
--1/2 += 5/1 = 9/2
-9/2 -= 5/1 = -1/2
--1/2 *= 5/1 = -5/2
--5/2 /= 5/1 = -5/10
-
------ Testing shorthand arithmetic assignment using integers
--1/3 += 3 = 8/3
-8/3 -= 3 = -1/3
--1/3 *= 3 = -3/3
--3/3 /= 3 = -3/9
-
------ Testing increment/decrement prefix and postfix
-Now g = -1/3
-g++ = -1/3
-Now g = 2/3
-++g = 5/3
-Now g = 5/3
-g-- = 5/3
-Now g = 2/3
---g = -1/3
-Now g = -1/3
 
  */
