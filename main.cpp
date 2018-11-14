2ae0cdc2e17b585537a103555160f01363b3a298
Mike-415 <mgonz160@mail.ccsf.edu>
2018-11-13 17:02:52 -0800

    Added >> operator

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@ public:
         ADD, SUBTRACT
     };
     friend std::ostream& operator<<(std::ostream& leftOS, const Fraction& rightOperand);
+    friend std::istream& operator>>(std::istream& leftIS, Fraction& rightOperand);
     friend bool operator<(const Fraction& leftOperand, const Fraction& rightOperand);
     friend bool operator>(const Fraction& leftOperand, const Fraction& rightOperand);
     friend bool operator<=(const Fraction& leftOperand, const Fraction& rightOperand);
@@ -40,7 +41,7 @@ private:
     int denominator;
     friend bool compareFractions(const Fraction& leftOperand, const Fraction& rightOperand, RelationalOperators type);
     friend Fraction addOrSubtract(const Fraction& leftOperand, const Fraction& rightOperand, ArithmeticOperators type);
-    void simplify(Fraction& original);
+    void simplify(Fraction& fraction);
 };
 
 
@@ -84,6 +85,101 @@ std::ostream& operator<<(std::ostream& leftOutput, const Fraction& rightOperand)
         leftOutput << rightOperand.numerator << "/" << rightOperand.denominator;
     }
     return leftOutput;
+}
+
+
+
+
+
+
+
+/*
+This operator must read exactly one fraction value from input.
+You should be able to read any of the formats described above
+(mixed number, negative number, whole numbers, etc.).
+You may assume that there are no spaces or formatting errors
+in the Fractions that you read. This means, for example,
+that in a mixed number only the whole number
+(not the numerator or denominator) may be negative, and that
+in a fraction with no whole number part only the numerator
+(not the denominator) may be negative.
+
+Note: You may need to exceed 15 lines for this function.
+My solution is about 20 lines long.
+
+Extraction Operator Hints:
+
+Since your extraction operator should not consume anything
+after the end of the Fraction being read, you will probably want to use
+the .peek() function to look ahead in the input stream and see what
+the next character is after the first number is read.
+If it's not either a '/'' or a '+', then you are done reading
+and should read no further. I have something like this:
+
+    int temp;
+    in >> temp;
+    if (in.peek() == '+'){
+        doSomething...
+    } else if (in.peek() == '/'){
+        doSomethingElse...
+    } else {
+        doThirdOption
+    }
+You don't need to detect or read the minus operator as a separate character.
+When you use the extraction operator to read an int, it will interpret a
+leading minus sign correctly. So, for example, you shouldn't have
+"if (in.peek() == '-')"
+
+The peek() function does not consume the character from the input stream,
+so after you use it to detect what the next character is,
+the first thing you need to do is get past that character.
+One good way to do that would be to use in.ignore(),
+which ignores one single character in the input stream.
+
+Here is one example of how processing will work:
+if the fraction in the input stream is a mixed number,
+you will need to have at least three input operations
+so that you can read all three integers in the mixed number.
+
+You can model the syntax of your operator>>() function header
+after the operator<<() function syntax you have seen in examples.
+Just use istream in the place of ostream
+(since we are doing input instead of output).
+Also, keep in mind that the right operand will NOT
+be const since the extraction operator's purpose
+is to modify the right operand.
+
+ */
+
+
+std::istream& operator>>(std::istream& leftIS, Fraction& rightOperand){
+    int firstPortion, secondPortion, thirdPortion;
+    leftIS >> firstPortion;
+    cout << "firstPortion: " << firstPortion << endl;
+    if (leftIS.peek() == '+'){
+        leftIS.ignore();
+        leftIS >> secondPortion; //Numerator
+        leftIS.ignore();
+        leftIS >> thirdPortion;  //Denominator
+        cout << "secondPortion: " << secondPortion << endl;
+        cout << "thirdPortion: " << thirdPortion << endl;
+
+        firstPortion = thirdPortion * firstPortion;
+        firstPortion += (firstPortion > 0)? secondPortion : -secondPortion;
+        secondPortion = thirdPortion;
+        //doSomething...
+    } else if (leftIS.peek() == '/'){
+        //doSomethingElse...
+        leftIS.ignore();
+        leftIS >> secondPortion;
+    } else {
+        //doThirdOption
+        secondPortion = 1;
+    }
+    Fraction temp(firstPortion, secondPortion);
+    cout << "temp: " << temp.numerator << "/" << temp.denominator << endl;
+    rightOperand = temp;
+    return leftIS;
 }
 
 
@@ -180,12 +276,10 @@ Fraction addOrSubtract(const Fraction& leftOperand, const Fraction& rightOperand
         case Fraction::SUBTRACT:
             newNumerator = leftNumerator - rightNumerator;
             break;
-        default:
-            throw std::invalid_argument("Invalid arithmetic operator type");
     }
     int newDenominator = leftOperand.denominator * rightOperand.denominator;
-    Fraction sum(newNumerator, newDenominator);
-    return sum;
+    Fraction result(newNumerator, newDenominator);
+    return result;
 }
 
 
@@ -328,16 +422,16 @@ Fraction Fraction::operator--(int){
 
 
 
-void Fraction::simplify(Fraction& original){
-    int absNumerator = std::abs(original.numerator);
-    int smallestNum = (absNumerator < original.denominator)? absNumerator : original.denominator;
-    while( smallestNum > 1 ){
-        if(absNumerator % smallestNum == 0 &&
-           original.denominator  % smallestNum == 0){
-            original.numerator /= smallestNum;
-            original.denominator /= smallestNum;
+void Fraction::simplify(Fraction& fraction){
+    int absNumerator = std::abs(fraction.numerator);
+    int smallestValue = (absNumerator < fraction.denominator)? absNumerator : fraction.denominator;
+    while( smallestValue > 1 ){
+        if(absNumerator % smallestValue == 0 &&
+           fraction.denominator  % smallestValue == 0){
+            fraction.numerator /= smallestValue;
+            fraction.denominator /= smallestValue;
         }
-        smallestNum--;
+        smallestValue--;
     }
 }
 
@@ -345,6 +439,10 @@ void Fraction::simplify(Fraction& original){
 
 
 
+
+//You need to figure out a way to solve the >> operator problem
+//1. Review the readings to see what's different in the function signature
+//2. Look at the rubric so you can use the pre-defined if/else statement
 
 
 
@@ -358,8 +456,12 @@ int main()
 {
     Fraction f1(-9,3);
     Fraction f2(-95, 45);
+    Fraction f3;
     cout << f1 << endl;
     cout << f2 << endl;
+    cin >> f3;
+    cout << "main: f3 = " << f3 << endl;
+
 }
 
 
